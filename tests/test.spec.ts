69e6504dbb49e302115b262964ed0c2b091630fb
jill64 <intents.turrets0h@icloud.com>
2023-11-11 00:07:51 +0900

    Update page URL in test.spec.ts

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test('basic test', async ({ page }) => {
-  await page.goto('/tailwind-grid-auto')
+  await page.goto('/')
   await expect(
     page.getByRole('heading', { name: 'tailwind-grid-auto' })
   ).toBeVisible()
