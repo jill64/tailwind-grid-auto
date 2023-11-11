9f6376e6ace5a209fd661ce2858a18c6f336a5cf
jill64 <intents.turrets0h@icloud.com>
2023-11-11 17:49:45 +0900

    Add dark mode support and apply font-sans to body element.

--- demo/tailwind.config.ts
+++ demo/tailwind.config.ts
@@ -3,7 +3,8 @@ import * as gridAuto from '../dist/index.js'
 
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  plugins: [gridAuto]
+  plugins: [gridAuto],
+  darkMode: 'class'
 } satisfies Config
 
 export default config
