068cbd98ca71f7133e30293e5ff0453ca09f7365
jill64 <143883742+jill64@users.noreply.github.com>
2023-11-22 02:47:14 +0000

    Update Sentry DSN in client and server hooks

--- demo/src/hooks.client.ts
+++ demo/src/hooks.client.ts
@@ -1,6 +1,5 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
 
-const onError = clientInit(PUBLIC_SENTRY_DSN)
+const onError = clientInit('https://8dfc36ba0b9e8643d4a16bf02132ee75@o4505814639312896.ingest.sentry.io/4506202206109696')
 
 export const handleError = onError()
