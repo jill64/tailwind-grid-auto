5daaedc0feb098aa1b13820cb8d2a3f31ced377d
jill64 <intents.turrets0h@icloud.com>
2023-12-04 13:59:46 +0900

    def dark theme in demo

--- demo/src/hooks.server.ts
+++ demo/src/hooks.server.ts
@@ -1,8 +1,9 @@
-import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
+import { onRender } from '@jill64/svelte-dark-theme'
 
-const { onHandle, onError } = serverInit(
+const { onHandle, onError } = init(
   'https://8dfc36ba0b9e8643d4a16bf02132ee75@o4505814639312896.ingest.sentry.io/4506202206109696'
 )
 
-export const handle = onHandle()
+export const handle = onHandle(onRender())
 export const handleError = onError()
