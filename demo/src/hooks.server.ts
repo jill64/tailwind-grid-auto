e6e6b078f8fae9a8d6dda893e0cc08ae3daad010
jill64 <intents.turrets0h@icloud.com>
2023-12-11 03:56:17 +0900

    chore: refactor demo with npm-demo-layout

--- demo/src/hooks.server.ts
+++ demo/src/hooks.server.ts
@@ -1,9 +1,9 @@
+import { hooks } from '@jill64/npm-demo-layout'
 import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
-import { onRender } from '@jill64/svelte-dark-theme'
 
 const { onHandle, onError } = init(
   'https://8dfc36ba0b9e8643d4a16bf02132ee75@o4505814639312896.ingest.sentry.io/4506202206109696'
 )
 
-export const handle = onHandle(onRender())
+export const handle = onHandle(hooks)
 export const handleError = onError()
